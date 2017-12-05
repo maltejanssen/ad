78f01ebad41576241be697e7c26fb7a752ea024d
unknown <dennis.sentler@haw-hamburg.de>
2017-12-05 22:06:34 +0100

    pos erweitert

--- ad07/src/impl/Pos.java
+++ ad07/src/impl/Pos.java
@@ -11,5 +11,25 @@ public class Pos<T extends Comparable<T>> {
 		idx = 0;
 		isValid = false;
 	}
+	
+	public Pos(int idx) {
+		container = null;
+		this.idx = idx;
+		isValid = true;
+	}
+	
+	public Pos(Container<T> cont) {
+		container = cont;
+		idx = 0;
+		isValid = true;
+	}
+	
+	public void setInvalid() {
+		isValid = false;
+	}
+	
+	public boolean isValid() {
+		return isValid;
+	}
 
 }
