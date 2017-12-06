22a32ddcd1e4a91510160e4e1a70a8dabc71093a
Janssen <Malte.Janssen@haw-hamburg.de>
2017-12-06 17:47:46 +0100

    add implemented in List version

--- ad08/src/impl/Adjazenzlists.java
+++ ad08/src/impl/Adjazenzlists.java
@@ -1,6 +1,8 @@
 package impl;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -10,21 +12,44 @@ public class Adjazenzlists extends Graph {
 	Map<Node,List<Node>> nodes = new HashMap<Node,List<Node>>();
 	Map<Node,List<Edge>> edges = new HashMap<Node,List<Edge>>();
 	
-	public Adjazenzlists(Set<Node> vertices, Set<Edge> edges) {
-		super(vertices, edges);
+	public Adjazenzlists(Set<Node> nodes, Set<Edge> edges) {
+		for (Node n : nodes) {
+			if(nodes.add(n))
+			{
+				this.add(n);
+			}
+		}
+		for (Edge e : edges) {
+			this.setEdge(e);
+		}
+	
 	}
 
 	@Override
 	public Pos add(Node node) {
+		if (!nodes.containsKey(node)) {
+			List<Node> nodeList = new ArrayList<Node>();
+			List<Edge> edgeList = new ArrayList<Edge>();
+			nodes.put(node, nodeList);
+			edges.put(node, edgeList);
+			
+		}
 		return null;
+		
 	}
 	
-	private boolean edgeExists(Node origin, Node destination) {
+	private boolean doesEdgeExists(Node origin, Node destination) {
 		if (origin.equals(destination)) {
 			return true;
 		}
-		
 		List<Edge> list = edges.get(origin);
+		if(list!=null) {
+			for (Edge e : list) {
+				if (e.getSecondNode() == destination) {
+					return true;
+				}
+			}
+		}
 		return false;
 	}
 
@@ -48,6 +73,12 @@ public class Adjazenzlists extends Graph {
 
 	@Override
 	public Set<Node> getNodes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator<Node> iterator() {
 		// TODO Auto-generated method stub
 		return null;
 	}
--- ad08/src/impl/Graph.java
+++ ad08/src/impl/Graph.java
@@ -5,23 +5,6 @@ import java.util.Set;
 
 public abstract class Graph implements Iterable<Node> {
 	
-	Set<Node> nodes;
-	
-	public Graph(Set<Node> nodes, Set<Edge> edges) {
-		for (Node n : nodes) {
-			if(nodes.add(n))
-			{
-				this.add(n);
-			}
-		}
-		
-		for (Edge e : edges) {
-			this.setEdge(e);
-		}
-		
-		
-	}
-	
 	public abstract Pos add(Node node);
 	
 	public abstract boolean setEdge(Edge edge);
@@ -31,10 +14,5 @@ public abstract class Graph implements Iterable<Node> {
 	public abstract List<Node> getNeighbours(Node node);
 	
 	public abstract Set<Node> getNodes();
-	
-	@Override
-	public Iterator<Node> iterator() {
-		return nodes.iterator();
-	}
 	
 }
